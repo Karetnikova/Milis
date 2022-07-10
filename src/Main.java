f644b18ce101a7a4069aa9f6bbbba496613a58eb
Karetnikova <Karetnikovala@mail.ru>
2022-07-10 17:37:26 +0800

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int price = 10_000;
+        int milis = price / 20;
+        System.out.println("Бонус: " + milis);
+
+    }
 }
