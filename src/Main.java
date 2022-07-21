ff03ef5ef733d1e98415b994c6f2b9e260a96b21
Karetnikova <Karetnikovala@mail.ru>
2022-07-21 20:19:36 +0800

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int milis = price / 20;
-        System.out.println("Бонус: " + milis);
-
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
